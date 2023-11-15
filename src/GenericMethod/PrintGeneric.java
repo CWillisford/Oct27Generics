884e1e00cb1d132b45d7d3f2aa94962c18627688
CWillisford <cwillisford@ggc.com>
2023-11-15 06:02:40 -0500

    11-15-2023 edits to PrintGenericTester

--- src/GenericMethod/PrintGeneric.java
+++ src/GenericMethod/PrintGeneric.java
@@ -1,16 +1,23 @@
 package GenericMethod;
 
+import GenericWildcard02.PrintList;
+
 import java.util.Arrays;
 import java.util.List;
 public class PrintGeneric {
 
     public <T> void print(List<T> list){
+        System.out.println("From generic method");
         System.out.println(list);
     }
 }
 
 class PrintGenericTester{
     public static void main(String[] args) {
-        List<String> strList = Arrays.asList("Truong","Elizabeth","Kevin");
+        List<String> strList = Arrays.asList("Dawson","Carson","Aaron","Rylan");
+        List<Integer> intList = Arrays.asList(1,2,3,4);
+        PrintGeneric pg = new PrintGeneric();
+        pg.print(strList);
+        pg.print(intList);
     }
 }
